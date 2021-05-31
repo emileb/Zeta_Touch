4f57e508ce39ee6b763ba9e7988db5ecf92189ba
Emile Belanger <emile.belanger@gmail.com>
2021-05-31 16:20:44 +0100

    Added addon games for SW

--- RazeTouch/app/src/main/java/com/opentouchgaming/razetouch/SwLauncher.java
+++ RazeTouch/app/src/main/java/com/opentouchgaming/razetouch/SwLauncher.java
@@ -24,5 +24,13 @@ public class SwLauncher extends RazeBaseLauncher
 
         SubGame.addGame(availableSubGames, getRunDirectory(), getSecondaryDirectory(), SUB_DIR, "", 0, WEAPON_WHEEL_NBR, new String[]{"sw.grp"}, R.drawable.raze,
                         "Shadow Warrior", "Copy SW.GRP to:", "Put your SW.GRP files here.txt");
+
+        SubGame sg = SubGame.addGame(availableSubGames, getRunDirectory(), getSecondaryDirectory(), SUB_DIR, "addons", 0, WEAPON_WHEEL_NBR, new String[]{"addons/td.grp"}, R.drawable.raze,
+                                     "Shadow Warrior: Twin Dragon", "Copy your TD.GRP file to:", "Put your TD.GRP here.txt");
+        sg.setExtraArgs("-gamegrp TD.GRP");
+
+        sg = SubGame.addGame(availableSubGames, getRunDirectory(), getSecondaryDirectory(), SUB_DIR, "addons", 0, WEAPON_WHEEL_NBR, new String[]{"addons/wt.grp"}, R.drawable.raze,
+                             "Shadow Warrior: Wanton Destruction", "Copy your WT.GRP file to:", "Put your WT.GRP here.txt");
+        sg.setExtraArgs("-gamegrp WT.GRP");
     }
 }
